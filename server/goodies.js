f47c818a97565e0af0dfe2f076aeb1bf404c5b95
Paolinni <pxv2009@gmail.com>
2016-02-27 08:21:16 -0600

    modified .gitignore for deployment

---
+++ server/goodies.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURL : 'mongodb://hanksucks:hanksucks@ds043615.mongolab.com:43615/guessts',
+  sendGridAuth : { user: 'paolinni', pass: 'goodenough27' },
+  amazonConfig : {accessKeyId: 'AKIAJ67KLSRHSVXDTXEQ', secretAccessKey: '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',region: 'us-west-2'}
+}
